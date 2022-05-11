antmicro/rowhammer-tester

WIP: Add ddr5_test_board target
Signed-off-by: Ryszard Różak <rrozak@antmicro.com>

rowhammer_tester/targets/ddr5_test_board.py
@@ -1,0 +1,99 @@
+#!/usr/bin/env python3
+
+import math
+
+from migen import *
+
+from litex.build.xilinx.vivado import vivado_build_args, vivado_build_argdict
+from litex.soc.integration.builder import Builder
+from litex.soc.cores.clock import S7PLL, S7IDELAYCTRL
+
+from litex_boards.platforms import antmicro_ddr5_test_board
+from litedram.phy import ddr5
+from liteeth.phy import LiteEthS7PHYRGMII
+
+from rowhammer_tester.targets import common
+
+# CRG ----------------------------------------------------------------------------------------------
+
+class CRG(Module):
+    def __init__(self, platform, sys_clk_freq, iodelay_clk_freq):
+        self.clock_domains.cd_sys    = ClockDomain()
+        self.clock_domains.cd_sys2x  = ClockDomain(reset_less=True)
+        self.clock_domains.cd_sys8x  = ClockDomain(reset_less=True)
+        self.clock_domains.cd_idelay = ClockDomain()
+
+        # # #
+
+        self.submodules.pll = pll = S7PLL(speedgrade=-1)
+        pll.register_clkin(platform.request("clk100"), 100e6)
+        pll.create_clkout(self.cd_sys,    sys_clk_freq)
+        pll.create_clkout(self.cd_sys2x,  2 * sys_clk_freq)
+        pll.create_clkout(self.cd_sys8x,  8 * sys_clk_freq)
+        pll.create_clkout(self.cd_idelay, iodelay_clk_freq)
+
+        self.submodules.idelayctrl = S7IDELAYCTRL(self.cd_idelay)
+
+# SoC ----------------------------------------------------------------------------------------------
+
+class SoC(common.RowHammerSoC):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def get_platform(self):
+        return antmicro_ddr5_test_board.Platform()
+
+    def get_crg(self):
+        return CRG(self.platform, self.sys_clk_freq,
+            iodelay_clk_freq=float(self.args.iodelay_clk_freq))
+
+    def get_ddrphy(self):
+        return ddr5.K7DDR5PHY(self.platform.request("ddr5"),
+            iodelay_clk_freq = float(self.args.iodelay_clk_freq),
+            sys_clk_freq     = self.sys_clk_freq)
+
+    def get_sdram_ratio(self):
+        return "1:8"
+
+    def add_host_bridge(self):
+        self.submodules.ethphy = LiteEthS7PHYRGMII(
+            clock_pads       = self.platform.request("eth_clocks"),
+            pads             = self.platform.request("eth"),
+            hw_reset_cycles  = math.ceil(float(self.args.eth_reset_time) * self.sys_clk_freq),
+            rx_delay         = 0.8e-9,
+            iodelay_clk_freq = float(self.args.iodelay_clk_freq),
+        )
+        self.add_etherbone(
+            phy          = self.ethphy,
+            ip_address   = self.ip_address,
+            mac_address  = self.mac_address,
+            udp_port     = self.udp_port,
+            buffer_depth = 256)
+
+# Build --------------------------------------------------------------------------------------------
+
+def main():
+    parser = common.ArgumentParser(
+        description  = "LiteX SoC on DDR5 Test Board",
+        sys_clk_freq = '50e6',
+        module       = 'MT60B2G8HB48B'
+    )
+    g = parser.add_argument_group(title="LPDDR4 Test Board")
+    parser.add(g, "--eth-reset-time", default="10e-3", help="Duration of Ethernet PHY reset")
+    parser.add(g, "--iodelay-clk-freq", default="200e6", help="IODELAY clock frequency")
+    vivado_build_args(g)
+    args = parser.parse_args()
+
+    soc_kwargs = common.get_soc_kwargs(args)
+    soc = SoC(**soc_kwargs)
+
+    target_name = 'ddr5_test_board'
+    builder_kwargs = common.get_builder_kwargs(args, target_name=target_name)
+    builder = Builder(soc, **builder_kwargs)
+    build_kwargs = vivado_build_argdict(args) if not args.sim else {}
+
+    common.run(args, builder, build_kwargs, target_name=target_name)
+
+if __name__ == "__main__":
+    main()
+

dwalton64/litex-rowhammer-tester

rowhammer_tester/targets/ddr5_test_board.py
@@ -1,0 +1,99 @@
+#!/usr/bin/env python3
+
+import math
+
+from migen import *
+
+from litex.build.xilinx.vivado import vivado_build_args, vivado_build_argdict
+from litex.soc.integration.builder import Builder
+from litex.soc.cores.clock import S7PLL, S7IDELAYCTRL
+
+from litex_boards.platforms import antmicro_ddr5_test_board
+from litedram.phy import ddr5
+from liteeth.phy import LiteEthS7PHYRGMII
+
+from rowhammer_tester.targets import common
+
+# CRG ----------------------------------------------------------------------------------------------
+
+class CRG(Module):
+    def __init__(self, platform, sys_clk_freq, iodelay_clk_freq):
+        self.clock_domains.cd_sys    = ClockDomain()
+        self.clock_domains.cd_sys2x  = ClockDomain(reset_less=True)
+        self.clock_domains.cd_sys8x  = ClockDomain(reset_less=True)
+        self.clock_domains.cd_idelay = ClockDomain()
+
+        # # #
+
+        self.submodules.pll = pll = S7PLL(speedgrade=-1)
+        pll.register_clkin(platform.request("clk100"), 100e6)
+        pll.create_clkout(self.cd_sys,    sys_clk_freq)
+        pll.create_clkout(self.cd_sys2x,  2 * sys_clk_freq)
+        pll.create_clkout(self.cd_sys8x,  8 * sys_clk_freq)
+        pll.create_clkout(self.cd_idelay, iodelay_clk_freq)
+
+        self.submodules.idelayctrl = S7IDELAYCTRL(self.cd_idelay)
+
+# SoC ----------------------------------------------------------------------------------------------
+
+class SoC(common.RowHammerSoC):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def get_platform(self):
+        return antmicro_ddr5_test_board.Platform()
+
+    def get_crg(self):
+        return CRG(self.platform, self.sys_clk_freq,
+            iodelay_clk_freq=float(self.args.iodelay_clk_freq))
+
+    def get_ddrphy(self):
+        return ddr5.K7DDR5PHY(self.platform.request("ddr5"),
+            iodelay_clk_freq = float(self.args.iodelay_clk_freq),
+            sys_clk_freq     = self.sys_clk_freq)
+
+    def get_sdram_ratio(self):
+        return "1:8"
+
+    def add_host_bridge(self):
+        self.submodules.ethphy = LiteEthS7PHYRGMII(
+            clock_pads       = self.platform.request("eth_clocks"),
+            pads             = self.platform.request("eth"),
+            hw_reset_cycles  = math.ceil(float(self.args.eth_reset_time) * self.sys_clk_freq),
+            rx_delay         = 0.8e-9,
+            iodelay_clk_freq = float(self.args.iodelay_clk_freq),
+        )
+        self.add_etherbone(
+            phy          = self.ethphy,
+            ip_address   = self.ip_address,
+            mac_address  = self.mac_address,
+            udp_port     = self.udp_port,
+            buffer_depth = 256)
+
+# Build --------------------------------------------------------------------------------------------
+
+def main():
+    parser = common.ArgumentParser(
+        description  = "LiteX SoC on DDR5 Test Board",
+        sys_clk_freq = '50e6',
+        module       = 'MT60B2G8HB48B'
+    )
+    g = parser.add_argument_group(title="DDR5 Test Board")
+    parser.add(g, "--eth-reset-time", default="10e-3", help="Duration of Ethernet PHY reset")
+    parser.add(g, "--iodelay-clk-freq", default="200e6", help="IODELAY clock frequency")
+    vivado_build_args(g)
+    args = parser.parse_args()
+
+    soc_kwargs = common.get_soc_kwargs(args)
+    soc = SoC(**soc_kwargs)
+
+    target_name = 'ddr5_test_board'
+    builder_kwargs = common.get_builder_kwargs(args, target_name=target_name)
+    builder = Builder(soc, **builder_kwargs)
+    build_kwargs = vivado_build_argdict(args) if not args.sim else {}
+
+    common.run(args, builder, build_kwargs, target_name=target_name)
+
+if __name__ == "__main__":
+    main()
+